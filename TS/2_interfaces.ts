741ddc88af168f611ef63866e2f40efba78c58a6
Brostacia <brostacia@gmail.com>
2020-10-20 14:15:06 +0700

    four

---
+++ TS/2_interfaces.ts
@@ -0,0 +1,79 @@
+//  знак ? значит необязательное поле
+// Rect это тип
+
+interface Rect {
+  readonly id: string;
+  color?: string;
+  size: {
+    width: number;
+    height: number;
+  };
+}
+
+const rect1: Rect = {
+  id: "1234",
+  size: {
+    width: 20,
+    height: 30,
+  },
+  color: "#ccc",
+};
+
+const rect2: Rect = {
+  id: "12345",
+  size: {
+    width: 10,
+    height: 5,
+  },
+};
+
+rect2.color = "black";
+// rect2.id = "3232"; нельзя, readonly
+
+const rect3 = {} as Rect;
+const rect4 = <Rect>{};
+
+// ========================
+
+interface RectWithArea extends Rect {
+  // после стрелки тип данных, кот дб возвр
+  getArea: () => number;
+}
+
+const rect5: RectWithArea = {
+  id: "123",
+  size: {
+    width: 20,
+    height: 20,
+  },
+  getArea(): number {
+    return this.size.width * this.size.height;
+  },
+};
+
+// =============
+
+// чаще называют черещ I (interface)
+interface IClock {
+  time: Date;
+  setTime(date: Date): void;
+}
+
+class Clock implements IClock {
+  time: Date = new Date();
+
+  setTime(date: Date): void {
+    this.time = date;
+  }
+}
+
+// =============
+interface Styles {
+  [key: string]: string;
+}
+
+const css: Styles = {
+  border: "1px solid black",
+  marginTop: "2px",
+  borderRadius: "5px",
+};
